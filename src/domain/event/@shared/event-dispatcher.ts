e3754f2d2dd8c8d1019f88d763cbc85c4ccac71b
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2024-03-11 14:49:58 -0300

    feat: register first event

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -3,13 +3,22 @@ import type EventHandlerInterface from './event-handler.interface'
 import type EventInterface from './event.interface'
 
 export default class EventDispatcher implements EventDispatcherInterface {
+  private readonly eventHandlers: Record<string, EventHandlerInterface[]> = {}
+
+  get getEventHandlers (): Record<string, EventHandlerInterface[]> {
+    return this.eventHandlers
+  }
+
   notify: (event: EventInterface) => void
 
   register (
     eventName: string,
     eventHandler: EventHandlerInterface<EventInterface>
   ): void {
-    //
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = []
+    }
+    this.eventHandlers[eventName].push(eventHandler)
   }
 
   unregister: (
